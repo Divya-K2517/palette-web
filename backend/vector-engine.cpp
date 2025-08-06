968f62c72b2123512561ac339d94121b5091c022
Divya Kumari <divya.k2517@gmail.com>
2025-08-06 01:13:32 -0400

    more work on vector-engine

--- backend/vector-engine.cpp
+++ backend/vector-engine.cpp
@@ -2,6 +2,11 @@
 #include "coreSystems/core-systems.hpp"
 #include <iostream>
 #include <curl/curl.h>
+#include <eigen3/Eigen/Dense>
+#include <algorithm>
+#include <future>
+#include <sstream>
+#include <cstdlib> //for std::getenv
 
 namespace coreSystems { 
     struct pinterestImage {
@@ -22,15 +27,16 @@ namespace coreSystems {
     class WeaviateClient { //for semantic search
     private:
         std::string baseUrl;
+        std::string apiKey; //for authentication
         CURL* curlHandle;
-        std mutex curlMutex; //to protect curl handle from concurrent access
+        std::mutex curlMutex; //to protect curl handle from concurrent access
         
         struct curlResponse { 
             std:: string data;
             long responseCode;
         };
 
-        static size_t writeCallback(void* contents, size_t size, size_t nmemb, CurlResponse* response) {
+        static size_t writeCallback(void* contents, size_t size, size_t nmemb, curlResponse* response) {
             //when data comes back from a request it will add it to curlResponse.data
             //nmemb is number of elements
             size_t totalSize = size * nmemb;
@@ -39,6 +45,87 @@ namespace coreSystems {
             return totalSize; //tells curl how many bytes were written
         }
     public: 
-        
+        explicit WeaviateClient(std:: string& baseUrl, const std::string& apiKey) //constructor
+            : baseUrl(baseUrl), apiKey(apiKey), curlHandle(nullptr) { //initialziing the baseUrl to the input and curlHandle to nullptr
+                curlHandle = curl_easy_init(); //creates a cURL, retuns nullptr if it fails
+                if (!curlHandle) { //throwing error if curlHandle is nullptr
+                    throw std::runtime_error("Failed to initialize CURL for WeaviateClient");
+                }
+            }
+        ~WeaviateClient() { //destructor 
+            if (curlHandle) { 
+                curl_easy_cleanup(curlHandle); //cleans up the curlHandle
+            }
+        }
+        // Function to perform a semantic search
+        //given a search string it will construct a GraphQL query
+        //post to weaviate endpoint
+        //parse results
+        //return a vector of Nodes
+        std::vector<Node> semanticSearch(const std::string& query) { 
+            //locking curlHandle with curlMutex
+            std::lockGuard<std::mutex> lock(curlMutex);
+            //constructing the GraphQL query
+            std::stringstream graphqlQuery;
+            graphqlQuery << R"({
+                "query": "{ Get { Concept(nearText: { concepts: [\")" << query << R"(\"] } limit: 10) { 
+                    name 
+                    description 
+                    _additional { 
+                        certainty 
+                        vector 
+                    } 
+                }}}"
+            })";
+            std::string postData = graphqlQuery.str();
+            std::cout << "Weaviate GraphQL Query: " << postData << std::endl;
+            
+            std::string url = baseUrl + "/v1/graphql";
+
+            curlResponse response; //curlResponse object to store data
+    
+            //configuring curl
+            curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str()); //setting target url
+            curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, postData.c_str()); //configures the postData query
+            curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeCallback); //to handle incoming data
+            curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, &response); //passes address of response to writeCallback 
+
+            std::cout << "Sending request to Weaviate: " << url << std::endl;
+            //setting headers
+            struct curl_slist* headers = nullptr; //linked list of headers
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+            if (!apiKey.empty()) { //adding API key to headers if it exists
+                std::string authHeader = "Authorization: Bearer " + apiKey;
+                headers = curl_slist_append(headers, authHeader.c_str());
+                std::cout << "added api key to curl headers" << std::endl;
+            }
+            curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, headers); //applies headers to curl request
+            //executin request
+            CURLcode res = curl_easy_perform(curlHandle);
+            curl_slist_free_all(headers); //frees memory used by headers
+                //RES VS RESPONSE:
+                //response is the curlResponse struct that stores data from weaviate and the response code
+                //res is a CURLcode that just indicates if the request worked, not the HTTP response code
+            //checking for errors
+            if (res != CURLE_OK || response.responseCode != 200) {
+                std::cerr << "Weaviate request failed: " << curl_easy_strerror(res) 
+                      << " (HTTP " << response.response_code << ")" << std::endl;
+            }
+
+            //getting HTTP response code
+            curl_easy_getinfo(curlHandle, CURLINFO_RESPONSE_CODE, &response.responseCode);
+            std::cout << "Weaviate Response Code: " << response.responseCode << std::endl;
+            std::cout << "Weaviate Response Data: " << response.data << std::endl;
+
+            //parsing JSON response
+            try {
+                auto jsonResponse = nlohmann::json::parse(response.data);
+                return parseWeaviateResponse(jsonResponse, query); //TODO: write parseWeaviateResponse
+            } catch (const std::exception& e) {
+                std::cerr << "Failed to parse Weaviate response: " << e.what() << std::endl;
+                return {};
+            }
+        }
+    private:
     };
 }
